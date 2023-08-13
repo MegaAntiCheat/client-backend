2ba8104cdc38e19fbc3d6d0b1c2bfd4b9ab82a87
Christopher Cha <ccha@ucsd.edu>
2023-08-12 22:04:35 -0700

    Case for err from check_missing_args() (#43)
    * Case for err from check_missing_args()

--- src/main.rs
+++ src/main.rs
@@ -106,19 +106,33 @@ async fn main() {
             // Add missing launch options to the localconfig.vdf for the current user.
             // This only sticks if steam is closed when the write occurs.
             let _ = opts.write_corrected_args_to_file();
-        } else if let Ok(missing_opts) = missing {
-            if !missing_opts.is_empty() {
-                tracing::error!(
-                    "Please add the following launch options to your TF2 to allow the MAC client to interface correctly with TF2."
-                );
-                tracing::error!("Missing launch options: {:?}", missing_opts);
-                if !(args.ignore_launch_options) {
-                    panic!(
-                        "Missing required launch options in TF2 for MAC to function. Aborting..."
+        } else {
+            match missing{
+                Ok(missing_opts) if !missing_opts.is_empty() => {
+                    tracing::warn!(
+                        "Please add the following launch options to your TF2 to allow the MAC client to interface correctly with TF2."
                     );
+                    tracing::warn!("Missing launch options: {:?}", missing_opts);
+                    if !(args.ignore_launch_options) {
+                        panic!(
+                            "Missing required launch options in TF2 for MAC to function. Aborting..."
+                        );
+                    }
+
                 }
-            } else {
-                tracing::info!("All required launch arguments are present!");
+
+                Ok(_) => {
+                    tracing::info!("All required launch arguments are present!");
+                }
+
+                Err(missing_opts_err) => {
+                    tracing::warn!("Failed to verify app launch options: {}", missing_opts_err);
+                    if !(args.ignore_launch_options) {
+                        panic!(
+                            "Missing required launch options in TF2 for MAC to function. Aborting..."
+                        );
+                    }
+                }
             }
         }
     }
