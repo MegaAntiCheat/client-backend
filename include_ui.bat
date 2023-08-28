6f61ee61f5ed369e6025f82051cb34ed2a5c98c1
Christopher Cha <ccha@ucsd.edu>
2023-08-27 20:55:09 -0700

    add check for updates to UI (#66)
    * add check for updates to UI in scripts

--- include_ui.bat
+++ include_ui.bat
@@ -1,21 +1,49 @@
 @echo off
+setlocal enabledelayedexpansion
 
-echo "Removing old files"
-del /s /q ui\*
-copy NUL ui\.gitkeep
-del /s /q ui_temp\*
-rmdir /s /q ui_temp
+set "UPDATE_FLAG=0"
 
-echo "Cloning and building UI"
-git clone https://github.com/MegaAntiCheat/MegaAntiCheat-UI ui_temp
-cd ui_temp
-call npm install -g pnpm
-call npm exec pnpm i
-call npm exec pnpm run build
+echo "Checking for UI repository"
+if exist ui\last_commit_hash.txt (
+    echo "Reading existing commit hash"
+    set /p LAST_COMMIT_HASH=<ui\last_commit_hash.txt
+) else (
+    echo "last_commit_hash.txt not found, setting to empty"
+    set LAST_COMMIT_HASH=""
+)
 
-robocopy /S dist ../ui
+echo "Fetching latest commit hash from GitHub API"
+for /f "tokens=2 delims=:" %%i in ('curl -s https://api.github.com/repos/MegaAntiCheat/MegaAntiCheat-UI/git/refs/heads/main ^| findstr /c:"\"sha\""') do set GITHUB_HASH=%%i
+set GITHUB_HASH=!GITHUB_HASH:~2,40!
 
-echo "Removing temp files..."
-cd ..
-del /s /q ui_temp\* 1>NUL
-rmdir /s /q ui_temp
+if "%LAST_COMMIT_HASH%"=="%GITHUB_HASH%" (
+    echo "No updates to the repository."
+    set "UPDATE_FLAG=1"
+)
+
+if "!UPDATE_FLAG!"=="0" (
+    echo "Removing old files"
+    del /s /q ui\*
+    copy NUL ui\.gitkeep
+    del /s /q ui_temp\*
+    rmdir /s /q ui_temp
+
+    echo "Cloning UI repository"
+    git clone --filter=tree:0 https://github.com/MegaAntiCheat/MegaAntiCheat-UI ui_temp
+
+    echo "Saving last commit hash"
+    cd ui_temp
+    git rev-parse HEAD > ../ui/last_commit_hash.txt
+
+    echo "Building UI"
+    call npm install -g pnpm
+    call npm exec pnpm i
+    call npm exec pnpm run build
+
+    robocopy /S dist ../ui
+
+    echo "Removing temp files..."
+    cd ..
+    del /s /q ui_temp\* 1>NUL
+    rmdir /s /q ui_temp
+)
--- include_ui.sh
+++ include_ui.sh
@@ -1,33 +1,58 @@
 #!/bin/bash
 
-echo "Removing old UI files"
-rm -rf ui/*
-touch ui/.gitkeep
-rm -rf ui_temp
+UPDATE_FLAG=0
 
-echo "Cloning and building UI"
-git clone https://github.com/MegaAntiCheat/MegaAntiCheat-UI ui_temp
-cd ui_temp
+echo "Checking for UI repository"
 
-if ! command -v pnpm &> /dev/null
-then
-    echo ""
-    echo "pnpm could not be found, install it? (Y/n)"
-    read install_pnpm
-
-    if [[ $install_pnpm =~ ^([yY](es)?)?$ ]]
-    then
-	sudo npm i -g pnpm
-    else
-	exit
-    fi
+if [ -e "ui/last_commit_hash.txt" ]; then
+    echo "Reading existing commit hash"
+    LAST_COMMIT_HASH=$(<ui/last_commit_hash.txt)
+else
+    echo "last_commit_hash.txt not found, setting to empty"
+    LAST_COMMIT_HASH=""
 fi
 
+echo "Fetching latest commit hash from GitHub API"
+GITHUB_HASH=$(curl -s https://api.github.com/repos/MegaAntiCheat/MegaAntiCheat-UI/git/refs/heads/main | grep sha | cut -d '"' -f 4)
 
-npm exec pnpm i
-npm exec pnpm run build
-cp dist/* ../ui/
+if [ "$LAST_COMMIT_HASH" == "$GITHUB_HASH" ]; then
+    echo "No updates to the repository."
+    UPDATE_FLAG=1
+fi
 
-echo "Removing temp files"
-cd ..
-rm -rf ui_temp
+if [ "$UPDATE_FLAG" -eq 0 ]; then
+    echo "Removing old UI files"
+    rm -rf ui/*
+    touch ui/.gitkeep
+    rm -rf ui_temp
+
+    echo "Cloning and building UI"
+    git clone --filter=tree:0  https://github.com/MegaAntiCheat/MegaAntiCheat-UI ui_temp
+    
+    echo "Saving last commit hash"
+    cd ui_temp || exit
+    git rev-parse HEAD > ../ui/last_commit_hash.txt
+
+    if ! command -v pnpm &> /dev/null
+    then
+        echo ""
+        echo "pnpm could not be found, install it? (Y/n)"
+        read install_pnpm
+
+        if [[ $install_pnpm =~ ^([yY](es)?)?$ ]]
+        then
+        sudo npm i -g pnpm
+        else
+        exit
+        fi
+    fi
+
+
+    npm exec pnpm i
+    npm exec pnpm run build
+    cp dist/* ../ui/
+
+    echo "Removing temp files"
+    cd ..
+    rm -rf ui_temp
+fi
