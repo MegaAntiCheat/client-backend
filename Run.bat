922c771b78a72559f24a67c4e3d3d25ae369d65b
Sam <61996812+MeatSh1eld@users.noreply.github.com>
2024-01-16 14:44:26 -0500

    simplafied run.bat

--- Run.bat
+++ Run.bat
@@ -1,13 +1,10 @@
 @echo off
 
-echo Checking for updates
-git remote show origin
-
 echo Running first.bat...
 call client-backend\include_ui.bat
 
-echo Changing directory to the Rust project...
-cd /d "%~dp0client-backend\"
+echo Checking for updates
+git remote show origin
 
 echo Running cargo build...
 cargo run
