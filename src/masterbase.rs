9dacac51c9bc3b93339c64d4a372309812368279
Bash <bash09@discord>
2024-03-03 14:17:20 +1100

    Upload bytes via websocket.

--- src/masterbase.rs
+++ src/masterbase.rs
@@ -1,9 +1,17 @@
-use std::{fmt::Display, sync::Arc};
+use std::{
+    fmt::{Debug, Display},
+    sync::Arc,
+};
 
+use event_loop::HandlerStruct;
+use futures::SinkExt;
 use reqwest::Response;
 use serde::Deserialize;
 use thiserror::Error;
-use tokio::sync::mpsc::Sender;
+use tokio::{net::TcpStream, sync::mpsc::Sender};
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
+
+use crate::state::MACState;
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -11,13 +19,36 @@ pub enum Error {
     Reqwest(#[from] reqwest::Error),
     #[error("Failed to parse url: {0}")]
     UrlParse(#[from] url::ParseError),
+    #[error("Websocket error: {0}")]
+    WebSocket(#[from] tokio_tungstenite::tungstenite::error::Error),
     #[error("Request failed: {0}")]
     Failed(String),
 }
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Deserialize)]
 struct SessionID {
     session_id: u128,
+    #[serde(skip)]
+    close: Option<Sender<()>>,
+}
+
+impl Drop for SessionID {
+    fn drop(&mut self) {
+        self.close.as_ref().map(|c| {
+            c.try_send(()).map_err(|e| {
+                tracing::error!(
+                    "Failed to request closing of session {}: {e}",
+                    self.session_id
+                );
+            })
+        });
+    }
+}
+
+impl Debug for SessionID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SessionID({})", self.session_id)
+    }
 }
 
 impl Display for SessionID {
@@ -26,23 +57,28 @@ impl Display for SessionID {
     }
 }
 
-#[derive(Debug)]
+pub type WebSocket = WebSocketStream<MaybeTlsStream<TcpStream>>;
 pub struct DemoSession {
-    close: Sender<()>,
     session_id: SessionID,
+    ws_client: WebSocket,
 }
 
-impl Drop for DemoSession {
-    fn drop(&mut self) {
-        self.close
-            .try_send(())
-            .map_err(|e| {
-                tracing::error!(
-                    "Failed to request closing of session {}: {e}",
-                    self.session_id
-                );
-            })
-            .ok();
+impl DemoSession {
+    /// # Errors
+    /// If the client failed to send the bytes
+    pub async fn send_bytes(
+        &mut self,
+        bytes: Vec<u8>,
+    ) -> Result<(), tokio_tungstenite::tungstenite::Error> {
+        self.ws_client
+            .send(tokio_tungstenite::tungstenite::Message::Binary(bytes))
+            .await
+    }
+}
+
+impl Debug for DemoSession {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "DemoSession({})", self.session_id)
     }
 }
 
@@ -65,41 +101,77 @@ pub async fn new_demo_session(
 ) -> Result<DemoSession, Error> {
     let params = [("api_key", &key), ("fake_ip", &fake_ip), ("map", &map)];
 
-    let url = reqwest::Url::parse_with_params(&format!("{host}/session_id"), params)?;
+    // Request to start session
+    let url = reqwest::Url::parse_with_params(&format!("http://{host}/session_id"), params)?;
     let response = reqwest::get(url).await?;
 
     if !response.status().is_success() {
         return Err(Error::Failed(response.text().await?));
     }
 
-    let session_id: SessionID = response.json().await?;
+    let mut session_id: SessionID = response.json().await?;
 
     let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    session_id.close = Some(tx);
 
-    tokio::task::spawn(async move {
-        rx.recv()
-            .await
-            .expect("Didn't get closing message from DemoSession.");
+    // Wait for the dropped `DemoSession` to tell it to close the session.
+    // When it receives a message over the channel, it makes the appropriate request.
+    {
+        let id = session_id.session_id;
+        let host = host.clone();
+        let key = key.clone();
+        tokio::task::spawn(async move {
+            rx.recv()
+                .await
+                .expect("Didn't get closing message from DemoSession.");
 
-        match force_close_session(host, key).await {
-            Ok(_) => tracing::info!("Closed session."),
-            Err(e) => tracing::error!("Failed to close session: {e:?}"),
-        }
-    });
+            match force_close_session(host, key).await {
+                Ok(_) => tracing::info!("Closed session {id}."),
+                Err(e) => tracing::error!("Failed to close session: {e:?}"),
+            }
+        });
+    }
+
+    tracing::info!("Opening websocket");
+
+    // Open Websocket
+    let params: [(&str, &str); 2] = [("api_key", &key), ("session_id", &session_id.to_string())];
+    let url = reqwest::Url::parse_with_params(&format!("ws://{host}/demos"), params)?;
+    let (ws_client, _) = tokio_tungstenite::connect_async(url).await?;
 
     Ok(DemoSession {
-        close: tx,
         session_id,
+        ws_client,
     })
 }
 
+/// Forcefully closes the demo session if one was open.
+/// If `response.status().is_success()`, then a session was closed.
+/// If the reponse was unsuccessful but `response.status().is_server_error()`
+/// is false, there was probably no session open to start with.
+///
 /// # Errors
 /// * Fails to parse Url (usually indicating a bad host or key was provided)
 /// * Web request failed
 pub async fn force_close_session(host: Arc<str>, key: Arc<str>) -> Result<Response, Error> {
     let params = [("api_key", &key)];
 
-    let url = reqwest::Url::parse_with_params(&format!("{host}/close_session"), params)?;
+    let url = reqwest::Url::parse_with_params(&format!("http://{host}/close_session"), params)?;
 
     Ok(reqwest::get(url).await?)
+}
+
+#[allow(clippy::module_name_repetitions)]
+pub struct MasterbaseHandler {}
+
+impl MasterbaseHandler {}
+
+impl<IM, OM> HandlerStruct<MACState, IM, OM> for MasterbaseHandler {
+    fn handle_message(
+        &mut self,
+        state: &MACState,
+        message: &IM,
+    ) -> Option<event_loop::Handled<OM>> {
+        todo!()
+    }
 }
