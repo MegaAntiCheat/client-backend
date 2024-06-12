996b14fe870f3bfddaf418390fd5154c21cb0d26
MaxoMC <87615947+Maxo6453@users.noreply.github.com>
2024-06-12 16:16:58 -0700

    Icon now only created on windows

--- build.rs
+++ build.rs
@@ -13,19 +13,25 @@ fn main() {
     {
         println!("cargo::rustc-cfg=feature=\"include-ui\"");
     }
-    // Create a new, empty icon collection
-    let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
-    // Read a PNG file from disk and add it to the collection
-    let file = std::fs::File::open("assets/icon.png").expect("Could not find assets/icon.png");
-    let image = ico::IconImage::read_png(file).expect("Could not read PNG file");
-    icon_dir.add_entry(
-        ico::IconDirEntry::encode(&image).expect("Could not add PNG file to icon collection"),
-    );
-    // Write the ICO file to disk
-    let file = std::fs::File::create("assets/icon.ico").expect("Could not create assets/icon.ico");
-    icon_dir
-        .write(file)
-        .expect("Could not write assets/icon.ico to disk.");
-    // Compile and link icon.rc
-    embed_resource::compile("assets/icon.rc", embed_resource::NONE);
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        println!("This is windows, creating icon.");
+        // Create a new, empty icon collection
+        let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
+        // Read a PNG file from disk and add it to the collection
+        let file = std::fs::File::open("assets/icon.png").expect("Could not find assets/icon.png");
+        let image = ico::IconImage::read_png(file).expect("Could not read PNG file");
+        icon_dir.add_entry(
+            ico::IconDirEntry::encode(&image).expect("Could not add PNG file to icon collection"),
+        );
+        // Write the ICO file to disk
+        let file =
+            std::fs::File::create("assets/icon.ico").expect("Could not create assets/icon.ico");
+        icon_dir
+            .write(file)
+            .expect("Could not write assets/icon.ico to disk.");
+        // Compile and link icon.rc
+        embed_resource::compile("assets/icon.rc", embed_resource::NONE);
+    } else {
+        println!("This is not windows, skipping icon creation.");
+    }
 }
